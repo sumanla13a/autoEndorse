37a1ad5abab5968e7a51ec6bde39f80be012f29f
sumanla13a <lamasuman2@gmail.com>
2017-04-03 22:58:36 -0500

    auto endorse chrome extension

---
+++ bg.js
@@ -0,0 +1,11 @@
+function injectTheScript() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // query the active tab, which will be only one tab
+        //and inject the script in it
+        chrome.tabs.executeScript(tabs[0].id, {file: "./bower_components/jquery/dist/jquery.min.js"}, () => {
+        	chrome.tabs.executeScript(tabs[0].id, {file: "endorser.js"});
+        });
+    });
+}
+
+document.getElementById('endorseButton').addEventListener('click', injectTheScript);
---
+++ endorser.js
@@ -0,0 +1,5 @@
+(function() {
+	'use strict';
+	$('[aria-controls="featured-skills-expanded"][aria-expanded="false"]').click();
+	$("[data-control-name=endorse]").each((a,b) => b.click(	));
+})();
